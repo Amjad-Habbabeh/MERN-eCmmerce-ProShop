b2d8a106a4630ddcb6e16e836ea45315fc6b4b1d
Amjad habbabeh <amjad.habbabeh@gmail.com>
2021-02-20 17:42:45 +0100

    codealong until fetching product from react section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,23 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 function App() {
-  return <>welcom to proshop</>;
+  return (
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route exact path="/" component={HomeScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
