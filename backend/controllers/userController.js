1dd6b61cbde03b91275d5542095fa85730d58279
Amjad habbabeh <amjad.habbabeh@gmail.com>
2021-03-02 13:59:09 +0100

    code along until checkout proccess part 2

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,7 +10,7 @@ const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user && (await user.matchPassword(password))) {
-    res.send({
+    res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
@@ -71,5 +71,30 @@ const getUserProfile = asyncHandler(async (req, res) => {
     throw new Error('Invalid email or password');
   }
 });
+// @desc update user profile
+// @route put/api/users/profile
+// @access Private
 
-export { authUser, getUserProfile, registerUser };
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile };
